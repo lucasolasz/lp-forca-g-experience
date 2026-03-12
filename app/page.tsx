41515e996edb6e1f731155bf1bd97f5bea5064a1
conuaz <lucas.olasz@basisti.com.br>
2026-03-12 17:28:39 -0300

    Inclusao do Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "@/components/Hero";
 import { Nav } from "@/components/Nav";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-brand-navy selection:bg-brand-accent selection:text-white">
       <Nav />
+      <Hero />
     </div>
   );
 }
